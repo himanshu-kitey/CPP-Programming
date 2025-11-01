72406d4225264fd72a994e00e9e8475f0ff88f54
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-02 02:05:35 +0530

    01_private_public_demo.cpp

---
+++ CLASS/SESSION_07/01_private_public_demo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Date_1
+{
+    int day;
+    int month;
+    int year;
+};
+
+class Date_2
+{
+    private:
+        int day;
+        int month;
+        int year;
+};
+
+int main(void)
+{
+    struct Date_1 myDate_1;
+    Date_2 myDate_2;
+
+    myDate_1.month = 10; //Ok
+    myDate_2.month = 10; //Not Ok
+
+    return 0;
+}
