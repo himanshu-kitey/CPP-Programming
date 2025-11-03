2b2e6d9c0c64a41227a1972b44781a6d119b756f
Himanshu Kitey <himanshu.kitey@tietoevry.com>
2025-11-03 19:34:50 +0530

    02_static_data_member.cpp

--- CLASS/SESSION_09/STATIC_3/02_static_data_members.cpp
+++ CLASS/SESSION_09/STATIC_3/02_static_data_members.cpp
@@ -36,3 +36,23 @@ int main(void)
 
     return 0;
 }
+
+
+// Comments:
+// Analysis static qualifier to data defination statement inside class has
+// the following effects:
+// 1) Time of allocation : static
+// 2) Selection of allowcation: Data / BSS
+// 3) Frequecny of allocation: once
+// 4) Visibility: static and non-static members functions of class
+// [We are not considering derived classes, friend function and friend classes as of now]
+// 5) Lifetime: Lifetime of program.
+
+// For Non-static data members:
+// 1) time of allocation = Time of allocation of object.
+// 2) Section of allocation == section of allocation of object.
+// 3) Frequency of allocation == Per object.
+// 4) Visibility == non-static member function of class.
+//          [static member function cannnot access non-static data members]
+// We are not considering derived classes, friend function and friedn class as of now].
+// Life time == Lifetime of Object.
